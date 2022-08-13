d2097144cb353199bd601e23a7d8abbfd55a90d0
aten88 <virtuoso_ten@mail.ru>
2022-08-13 19:56:54 +0300

    Переписал заново функцию для закрепления и перезалил на GIT

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,17 +3,18 @@ def binary_search(list_search:list, item:int):
     high = len(list_search) - 1
 
     while low <= high:
-        mid = int((low + high)/2)
+        mid = int((low + high) / 2)
         guess = list_search[mid]
+
         if guess == item:
             return mid
-        if guess > item:
-            high = mid - 1
 
+        elif guess > item:
+            high = mid - 1
         else:
             low = mid + 1
-    return None
+    return "Элемент не найден"
 
-my_list = [1,7,19,22,157,212,222]
+my_list = [0,2,4,6,8,10,20,50,100]
 
-print(f'Необходимый эл-т под индексом: {binary_search(my_list, 222)}')
+print(f'Необходимый эл-т под индексом: {binary_search(my_list, 111)}')
